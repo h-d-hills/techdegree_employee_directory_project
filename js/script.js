54a6f67fc2bb748762f6469389a4fd7423f59567
Henry D. Hills <henry.d.hills@gmail.com>
2020-07-06 08:25:28 -0400

    Adding SCSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 let employees = [];
-const gridContainer = document.getElementById('grid-container');
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.getElementById("grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
 
-fetch('https://randomuser.me/api/')
+fetch(urlAPI)
     .then(res => res.json())
     .then(res => res.results)
     .then(displayEmployees)
@@ -11,14 +16,14 @@ fetch('https://randomuser.me/api/')
 function displayEmployees(employeeData){
     employees = employeeData;
     let employeeHTML = ``;
-    employees.forEach(employee => {
+    employees.forEach((employee, index) => {
         let name = employee.name;
         let email = employee.email;
         let city = employee.location.city;
         let picture = employee.picture;
         employeeHTML += `
-        <div class="card">
-            <img src="${picture.large}" alt="" class="avatar">
+        <div class="card" data-index="${index}">
+            <img src="${picture.large}" alt="" class="avatar"/>
             <div class="text-container">
                 <h2 class="name">${name.first} ${name.last}</h2>
                 <p class="email">${email}</p>
@@ -27,5 +32,47 @@ function displayEmployees(employeeData){
         </div>
         `;
     });
-    gridContainer.innerHTML += employeeHTML;
+    gridContainer.innerHTML = employeeHTML;
 }
+
+function displayModal(index) {
+    let {
+        name, 
+        dob, 
+        phone, 
+        email, 
+        location: {
+            city, 
+            street, 
+            state, 
+            postcode
+        }, 
+        picture
+    } = employees[index];
+    let date = new Date(dob.date);
+    let modalHTML = `
+        <img class='avatar' alt='' src='${picture.large}'/>
+        <div class='text-container'>
+            <h2 class='name'>${name.first} ${name.last}</h2>
+            <p class='email'>${email}</p>
+            <p class='address'>${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    `;
+    console.log(modalHTML);
+    overlay.classList.remove('hidden');
+    modalContainer.innerHTML = modalHTML;    
+}
+
+gridContainer.addEventListener('click', e=>{
+    if(e.target !== gridContainer){
+        displayModal(e.target.closest('.card').getAttribute('data-index'));
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
